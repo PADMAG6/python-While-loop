5acfdd47f29b05964804811aa6c728619c14f824
PADMAG6 <padmavathi.g6@gmail.com>
2021-01-28 16:52:12 +0530

    Create file1.py

---
+++ file1.py
@@ -0,0 +1,43 @@
+'''price = 1000000
+has_good_credit = True
+if has_good_credit:
+    down_payment = 0.1 * price
+else:
+    down_payment= 0.2 * price
+print(f"Down Pament: ${down_payment}")'''
+
+#temperature = 35
+#if temperature < 30   print("It's a hot day")#else:
+#    print("It's not a hot day")
+'''name = "Pd"
+if len(name) < 3:
+    print("name must be atleast 3 chart")
+elif len(name) > 50:
+    print("name can be max of 50 character")
+else:
+    print("name looks good!")
+'''
+'''weight = int(input("weight: "))
+unit = input ("(L)bs or (K)g: ")
+if unit.upper() == "L":
+    converted = weight * 0.45
+    print(f"You are {converted} kilos")
+else:
+    converted = weight /0.45
+    print(f"You are {converted} pounds")'''
+'''i = 1
+while i <= 5:
+    print('*' * i)
+    i += 1
+print("Done")'''
+'''secret_number = 9
+i = 0
+guess_limit = 3
+while i < guess_limit:
+    guess = int(input('Guess: '))
+    i += 1
+    if guess == secret_number:
+        print('You Won!')
+        break
+else:
+    print("failed, sorry")'''
