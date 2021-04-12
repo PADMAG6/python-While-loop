445c1bd0480c11277643417f2c73122c42c42927
PADMAG6 <padmavathi.g6@gmail.com>
2021-04-12 17:20:44 +0530

    Create python_samples.py

---
+++ python_samples.py
@@ -0,0 +1,30 @@
+//fibonacci 
+q= int(input("enter input terms: "))
+n1, n2 = 0,1
+count = 0
+if q <= 0:
+    print("go for next process")
+elif q == 1:
+    print("fibnonserice process up to" + q + "term")
+    print(n1)
+else:
+    print("fibanonserices")
+    while count < q:
+        print("s")
+        w = n1 + n2
+        n1 = n2
+        n2 = w
+        #print("q")
+        count += 1
+        
+        
+ //palindrom
+q=str(input("enter input terms: "))
+#print(q[::-1])
+#q1 = reversed(q)
+if q == q[::-1]:
+    print("palindrom")
+else:
+    print("not palindrom")
+        
+        
